6eeb325ce58fc5d57d7423afdd64aed0b70f168b
andreyoala <andreyoala@icloud.com>
2022-04-21 17:08:52 +0300

    Add some method.

--- MyFirstProduct/ViewController.swift
+++ MyFirstProduct/ViewController.swift
@@ -13,6 +13,12 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
+    func newMethod() {
+        print("Mother Fucker")
+    }
+    func anotherMethod() {
+
+    }
 
 
 }
